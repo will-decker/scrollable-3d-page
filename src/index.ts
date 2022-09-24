5e3fe75760e7ae0e86aea73c5c4b3b55286217dd
William Decker <willdecker@gmail.com>
2022-09-24 15:21:42 -0400

    add 3d drone, build nav and add header content

--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,84 @@
 import {
-    ViewerApp,
-    AssetManagerPlugin,
-    GBufferPlugin,
-    timeout,
-    ProgressivePlugin,
-    TonemapPlugin,
-    SSRPlugin,
-    SSAOPlugin,
-    DiamondPlugin,
-    FrameFadePlugin,
-    GLTFAnimationPlugin,
-    GroundPlugin,
-    BloomPlugin,
-    TemporalAAPlugin,
-    AnisotropyPlugin,
-    GammaCorrectionPlugin,
+  ViewerApp,
+  AssetManagerPlugin,
+  GBufferPlugin,
+  timeout,
+  ProgressivePlugin,
+  TonemapPlugin,
+  SSRPlugin,
+  SSAOPlugin,
+  DiamondPlugin,
+  FrameFadePlugin,
+  GLTFAnimationPlugin,
+  GroundPlugin,
+  BloomPlugin,
+  TemporalAAPlugin,
+  AnisotropyPlugin,
+  GammaCorrectionPlugin,
+  addBasePlugins,
+  ITexture,
+  TweakpaneUiPlugin,
+  AssetManagerBasicPopupPlugin,
+  CanvasSnipperPlugin,
+  IViewerPlugin,
 
-    addBasePlugins,
-    ITexture, TweakpaneUiPlugin, AssetManagerBasicPopupPlugin, CanvasSnipperPlugin,
+  // Color, // Import THREE.js internals
+  // Texture, // Import THREE.js internals
+} from 'webgi';
+import './styles.css';
 
-    IViewerPlugin,
+async function setupViewer() {
+  // Initialize the viewer
+  const viewer = new ViewerApp({
+    canvas: document.getElementById('webgi-canvas') as HTMLCanvasElement,
+    isAntialiased: true,
+  });
 
-    // Color, // Import THREE.js internals
-    // Texture, // Import THREE.js internals
-} from "webgi";
-import "./styles.css";
+  // Add some plugins
+  const manager = await viewer.addPlugin(AssetManagerPlugin);
 
-async function setupViewer(){
+  // Add a popup(in HTML) with download progress when any asset is downloading.
+  await viewer.addPlugin(AssetManagerBasicPopupPlugin);
 
-    // Initialize the viewer
-    const viewer = new ViewerApp({
-        canvas: document.getElementById('webgi-canvas') as HTMLCanvasElement,
-    })
+  // Add plugins individually.
+  // await viewer.addPlugin(GBufferPlugin)
+  // await viewer.addPlugin(new ProgressivePlugin(32))
+  //   await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm));
+  // await viewer.addPlugin(GammaCorrectionPlugin)
+  // await viewer.addPlugin(SSRPlugin)
+  // await viewer.addPlugin(SSAOPlugin)
+  // await viewer.addPlugin(DiamondPlugin)
+  // await viewer.addPlugin(FrameFadePlugin)
+  // await viewer.addPlugin(GLTFAnimationPlugin)
+  // await viewer.addPlugin(GroundPlugin)
+  // await viewer.addPlugin(BloomPlugin)
+  // await viewer.addPlugin(TemporalAAPlugin)
+  // await viewer.addPlugin(AnisotropyPlugin)
 
-    // Add some plugins
-    const manager = await viewer.addPlugin(AssetManagerPlugin)
+  // or use this to add all main ones at once.
+  await addBasePlugins(viewer);
 
-    // Add a popup(in HTML) with download progress when any asset is downloading.
-    await viewer.addPlugin(AssetManagerBasicPopupPlugin)
+  // Add more plugins not available in base, like CanvasSnipperPlugin which has helpers to download an image of the canvas.
+  await viewer.addPlugin(CanvasSnipperPlugin);
 
-    // Add plugins individually.
-    // await viewer.addPlugin(GBufferPlugin)
-    // await viewer.addPlugin(new ProgressivePlugin(32))
-    // await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
-    // await viewer.addPlugin(GammaCorrectionPlugin)
-    // await viewer.addPlugin(SSRPlugin)
-    // await viewer.addPlugin(SSAOPlugin)
-    // await viewer.addPlugin(DiamondPlugin)
-    // await viewer.addPlugin(FrameFadePlugin)
-    // await viewer.addPlugin(GLTFAnimationPlugin)
-    // await viewer.addPlugin(GroundPlugin)
-    // await viewer.addPlugin(BloomPlugin)
-    // await viewer.addPlugin(TemporalAAPlugin)
-    // await viewer.addPlugin(AnisotropyPlugin)
+  // This must be called once after all plugins are added.
+  viewer.renderer.refreshPipeline();
 
-    // or use this to add all main ones at once.
-    await addBasePlugins(viewer)
+  await manager.addFromPath('./assets/drone.glb');
 
-    // Add more plugins not available in base, like CanvasSnipperPlugin which has helpers to download an image of the canvas.
-    await viewer.addPlugin(CanvasSnipperPlugin)
+  viewer.getPlugin(TonemapPlugin)!.config!.clipBackground = true;
 
-    // This must be called once after all plugins are added.
-    viewer.renderer.refreshPipeline()
+  // Load an environment map if not set in the glb file
+  // await viewer.scene.setEnvironment(
+  //     await manager.importer!.importSinglePath<ITexture>(
+  //         "./assets/environment.hdr"
+  //     )
+  // );
 
-    await manager.addFromPath("./assets/classic-watch.glb")
-
-    // Load an environment map if not set in the glb file
-    // await viewer.scene.setEnvironment(
-    //     await manager.importer!.importSinglePath<ITexture>(
-    //         "./assets/environment.hdr"
-    //     )
-    // );
-
-    // Add some UI for tweak and testing.
-    const uiPlugin = await viewer.addPlugin(TweakpaneUiPlugin)
-    // Add plugins to the UI to see their settings.
-    uiPlugin.setupPlugins<IViewerPlugin>(TonemapPlugin, CanvasSnipperPlugin)
-
+  // Add some UI for tweak and testing.
+  //   const uiPlugin = await viewer.addPlugin(TweakpaneUiPlugin);
+  // Add plugins to the UI to see their settings.
+  uiPlugin.setupPlugins<IViewerPlugin>(TonemapPlugin, CanvasSnipperPlugin);
 }
 
-setupViewer()
+setupViewer();
